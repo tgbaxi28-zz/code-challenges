40619522c9d73887c6b7a6dd7439de8a77f78745
Tanmay Baxi <tgbaxi28@users.noreply.github.com>
2020-02-04 18:04:43 +0530

    Create all permutations of a string in Java (Sliding window + Recursion)

---
+++ GenerateAnagram.java
@@ -0,0 +1,19 @@
+/* Generate all permutations of a string */
+public class GenerateAnagram {
+  public static void main(String[] args) {
+    String s = "ABC";
+    permutations("", s);
+  }
+
+  private static void permutations(String candidate, String remaining) {
+    if (remaining.length() == 0) {
+      System.out.println(candidate);
+    }
+
+    for (int i = 0; i < remaining.length(); i++) {
+      String newCandidate = candidate + remaining.charAt(i);
+      String newRemaining = remaining.substring(0, i) + remaining.substring(i + 1);
+      permutations(newCandidate, newRemaining);
+    }
+  }
+}
