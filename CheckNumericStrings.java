8f8bcf9e9be9680d9cbd8db53b51e2661104bfa9
Tanmay Baxi <tgbaxi28@users.noreply.github.com>
2020-02-04 11:45:59 +0530

    Create CheckNumericStrings.java

---
+++ CheckNumericStrings.java
@@ -0,0 +1,57 @@
+/**
+ * Given a string, return whether it represents a number.
+ *
+ * <p>Here are the different kinds of numbers:
+ *
+ * <p>"10", a positive integer
+ *
+ * <p>"-10", a negative integer
+ *
+ * <p>"10.1", a positive real number
+ *
+ * <p>"-10.1", a negative real number
+ *
+ * <p>"1e5", a number in scientific notation
+ *
+ * <p>And here are examples of non-numbers:
+ *
+ * <p>"a" "x 1" "a -2" "-"
+ */
+public class CheckNumericStrings {
+
+  public static void main(String[] args) {
+    System.out.println(isNumeric("10"));
+    System.out.println(isNumeric("-10"));
+    System.out.println(isNumeric("10.1"));
+    System.out.println(isNumeric("-10.1"));
+    System.out.println(isNumeric("1e5"));
+    System.out.println(isNumeric("a"));
+    System.out.println(isNumeric("x 1"));
+    System.out.println(isNumeric("a-2"));
+    System.out.println(isNumeric("-"));
+    System.out.println(isNumeric("+10"));
+  }
+
+  // for all kinds of values like hexa decimal and all -- use NumberUtils.isCreatable instead of the
+  // custom method.
+  private static boolean isNumeric(String numericStr) {
+    System.out.print("" + numericStr + " is a Numeric String? ");
+    if (numericStr.isEmpty()) {
+      return false;
+    }
+    final char[] numericCharacters = numericStr.toCharArray();
+    // check if the string starts with any signs or not
+    int index = numericCharacters[0] == '-' || numericCharacters[0] == '+' ? 1 : 0;
+    boolean isNumeric = Boolean.FALSE;
+    // normal string numeric string + scientific
+    for (; index < numericCharacters.length; index++) {
+      if (Character.isDigit(numericCharacters[index])
+          || (index != 0 && (numericCharacters[index] == 'e' || numericCharacters[index] == '.'))) {
+        isNumeric = Boolean.TRUE;
+      } else {
+        return Boolean.FALSE;
+      }
+    }
+    return isNumeric;
+  }
+}
